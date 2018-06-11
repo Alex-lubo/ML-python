d3bebdf232d7523e7bd6c1c7620147626808d030
alex.lu <lubo@v-secure.cn>
2018-06-11 18:33:28 +0800

    finish cnn.

--- converlution-nn.py
+++ converlution-nn.py
@@ -2,10 +2,12 @@
 
 """
 实现一个简单的CNN，包含2个卷积层和1个全连接层。
-测试数据采用mnist数据
+测试数据采用mnist数据.
+采用2层conv网络和2层全连接网络，学习率0.001，训练2000次，可以获得98.9%的准确率
 """
 import sys
 import tensorflow as tf
+import numpy as np
 from tensorflow.examples.tutorials.mnist import input_data
 
 mnist = input_data.read_data_sets('MNIST_data',one_hot=True)
@@ -16,15 +18,15 @@ def input_train():
   """
   add your input procedure to import training data
   """
-  batch = mnist.train.next_batch(100)
-  x = tf.reshape(batch[0], [-1, 28, 28, 1])
+  batch = mnist.train.next_batch(50)
+  x = np.reshape(batch[0], [-1, 28, 28, 1])
   return x, batch[1]
 
 def input_test():
   """
   add your input procedure to import testing data
   """
-  test_x = tf.reshape(mnist.test.images, [-1, 28, 28, 1])
+  test_x = np.reshape(mnist.test.images, [-1, 28, 28, 1])
   return test_x, mnist.test.labels
 
 def weight_initializer(shape):
@@ -54,17 +56,17 @@ def max_pool_2x2(x):
 def main(argv=None):
   if argv is None:
     argv = sys.argv
+
   x_width = 28    # 输入图片的宽度
   x_height = 28   # 输入图片的高度
   x_channel = 1   # 输入图片的颜色通道数
   class_num = 10  # 样本种类
 
-
-  x = tf.placeholder(tf.float32, [-1, x_width, x_height, x_channel])
+  x = tf.placeholder(tf.float32, [None, x_width, x_height, x_channel])
   y_ = tf.placeholder(tf.float32, [None, class_num])
   keep_prob = tf.placeholder(tf.float32)
 
-  with tf.name_scope('Converlution Layer 1'):
+  with tf.name_scope('Converlution_Layer_1'):
     conv1_k_size = 5      # 卷积核大小 5x5
     conv1_filter_num = 32 # 滤波器个数，也就是想要抽取的特征数
     conv1_channel = x_channel
@@ -74,18 +76,17 @@ def main(argv=None):
     pool_conv1 = max_pool_2x2(h_conv1)                     # pool层
     map_size = x_width/2
 
-  with tf.name_scope('Converlution Layer 2'):
+  with tf.name_scope('Converlution_Layer_2'):
     conv2_k_size = 5
     conv2_filter_num = 64
     conv2_channel = conv1_filter_num
     W_conv2 = weight_initializer([conv2_k_size, conv2_k_size, conv2_channel, conv2_filter_num])
-    b_conv2 = bias_initializer([conv2_channel])
+    b_conv2 = bias_initializer([conv2_filter_num])
     h_conv2 = tf.nn.relu(conv2d(pool_conv1, W_conv2) + b_conv2)
     pool_conv2 = max_pool_2x2(h_conv2)
-    map_size = x_width/2
-    
+    map_size = map_size/2
 
-  with tf.name_scope('Fully Connected 1'):
+  with tf.name_scope('Fully_Connected_1'):
     nodes_num_fc1 = 1024    # 隐含节点数
     features_size_fc1 = map_size * map_size * conv2_filter_num
     W_fc1 = weight_initializer([features_size_fc1, nodes_num_fc1])
@@ -94,28 +95,32 @@ def main(argv=None):
     h_fc1 = tf.nn.relu(tf.matmul(pool2_flat, W_fc1) + b_fc1)
     drop_fc1 = tf.nn.dropout(h_fc1, keep_prob)
 
-  with tf.name_scope('Softmax Regression'):
+  with tf.name_scope('Softmax_Regression'):
     W_fc2 = weight_initializer([nodes_num_fc1, class_num])
     b_fc2 = bias_initializer([class_num])
     y_predict = tf.nn.softmax(tf.matmul(drop_fc1, W_fc2) + b_fc2)
 
   with tf.name_scope('Training'):
-    learning_rate = 1e-4
-    training_steps = 1000
-    loss_corss_entropy = tf.reduce_mean(tf.reduce_sum(-y_ * tf.log(y_predict)))
+    learning_rate = 1e-3
+    training_steps = 2000
+    loss_corss_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_predict), reduction_indices=[1]))
     train_step = tf.train.AdamOptimizer(learning_rate).minimize(loss_corss_entropy)
 
-  prediction = tf.equal(tf.argmax(y_predict, 1), tf.argmax(y_, 1))
-  accuracy = tf.reduce_mean(tf.cast(prediction, tf.float32))
-
+  with tf.name_scope('Evaluation'):
+    prediction = tf.equal(tf.argmax(y_predict, 1), tf.argmax(y_, 1))
+    accuracy = tf.reduce_mean(tf.cast(prediction, tf.float32))
 
   tf.global_variables_initializer().run()
   for i in range(training_steps):
     train_x, train_y = input_train()
-    if i%100 == 0:
-      train_accuracy = accuracy.eval(feed_dict={x: train_x, y_: train_y, keep_prob:1.0})
-      print("step %d, training accuracy is  %g" % (i, train_accuracy))
+    if i % 100 == 0:
+      print(
+        'step %d accuracy is %g' %(i, accuracy.eval(feed_dict={x: train_x, y_: train_y, keep_prob: 1.}))
+      )
     train_step.run(feed_dict={x: train_x, y_: train_y, keep_prob:0.75})
 
   test_x, test_y = input_test()
   print('test accuracy is %g' % accuracy.eval(feed_dict={x: test_x, y_: test_y, keep_prob: 1.0}))
+
+if __name__ == '__main__':
+  main()
--- multilayerPerceptron.py
+++ multilayerPerceptron.py
@@ -3,6 +3,7 @@
 """
 多层感知机(MLP)模型
 在Softmax Regression的基础上增加了隐藏层，相当于增加了特征抽象的过程，提高匹配和分类的正确率。
+使用mnist数据测试，按照参数，可以获得97%的正确率
 """
 
 import tensorflow as tf
@@ -25,45 +26,49 @@ def input_test():
   """
   return mnist.test.images, mnist.test.labels
 
-# define paramaters
-in_uint = 784
-h1_uint = 300
-class_num = 10
+def main():
+  # define paramaters
+  in_uint = 784
+  h1_uint = 300
+  class_num = 10
 
-# input data holder variables
-x = tf.placeholder(tf.float32, [None, in_uint], name='input_x')
-y_ = tf.placeholder(tf.float32, [None, class_num], name='input_y') # labels
-keep_prob = tf.placeholder(tf.float32)
+  # input data holder variables
+  x = tf.placeholder(tf.float32, [None, in_uint], name='input_x')
+  y_ = tf.placeholder(tf.float32, [None, class_num], name='input_y') # labels
+  keep_prob = tf.placeholder(tf.float32)
 
-# hyper-parameters
-W1 = tf.Variable(tf.truncated_normal([in_uint, h1_uint], stddev=0.1), name='weight_lay1')
-b1 = tf.Variable(tf.zeros([h1_uint]))
-W2 = tf.Variable(tf.truncated_normal([h1_uint, class_num], stddev=0.1), name='weight_lay2')
-b2 = tf.Variable(tf.zeros([class_num]))
+  # hyper-parameters
+  W1 = tf.Variable(tf.truncated_normal([in_uint, h1_uint], stddev=0.1), name='weight_lay1')
+  b1 = tf.Variable(tf.zeros([h1_uint]))
+  W2 = tf.Variable(tf.truncated_normal([h1_uint, class_num], stddev=0.1), name='weight_lay2')
+  b2 = tf.Variable(tf.zeros([class_num]))
 
-# define the computation layers and loss function
-hidden1 = tf.nn.relu(tf.matmul(x, W1) + b1)
-hidden_drop = tf.nn.dropout(hidden1, keep_prob)
-y = tf.nn.softmax(tf.matmul(hidden_drop, W2) + b2)
-loss = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y)))
+  # define the computation layers and loss function
+  hidden1 = tf.nn.relu(tf.matmul(x, W1) + b1)
+  hidden_drop = tf.nn.dropout(hidden1, keep_prob)
+  y = tf.nn.softmax(tf.matmul(hidden_drop, W2) + b2)
+  loss = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
 
-# define the training function
-learning_rate = 0.3
-training_steps = 1000
-train_step = tf.train.AdagradOptimizer(learning_rate).minimize(loss)
+  # define the training function
+  learning_rate = 0.3
+  training_steps = 1000
+  train_step = tf.train.AdagradOptimizer(learning_rate).minimize(loss)
 
-prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-accuracy = tf.reduce_mean(tf.cast(prediction, tf.float32))
+  prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+  accuracy = tf.reduce_mean(tf.cast(prediction, tf.float32))
 
-tf.global_variables_initializer().run()
-for i in range(training_steps):
-  batch_x, batch_y = input_train()
-  if i % 100 == 0:
-    print(
-      'step %d accuracy is %g' %(i, accuracy.eval(feed_dict={x: batch_x, y_: batch_y, keep_prob: 1.}))
-    )
-  train_step.run(feed_dict={x: batch_x, y_: batch_y, keep_prob: 0.75})
+  tf.global_variables_initializer().run()
+  for i in range(training_steps):
+    batch_x, batch_y = input_train()
+    if i % 100 == 0:
+      print(
+        'step %d accuracy is %g' %(i, accuracy.eval(feed_dict={x: batch_x, y_: batch_y, keep_prob: 1.}))
+      )
+    train_step.run(feed_dict={x: batch_x, y_: batch_y, keep_prob: 0.75})
 
-# evalution
-test_x, test_y = input_test()
-print('total accracy: %g',accuracy.eval(feed_dict={x: test_x, y_:test_y, keep_prob: 1.0}))
+  # evalution
+  test_x, test_y = input_test()
+  print('total accracy: %g',accuracy.eval(feed_dict={x: test_x, y_:test_y, keep_prob: 1.0}))
+
+if __name__ == "__main__":
+  main()
